effb5876633abeb48ef2beccbc6a93c7aec1a0a0
Stino <linksonder@gmail.com>
2018-02-02 14:57:59 +0100

    Eerste versie aan de praat

--- src/app/client/app.component.ts
+++ src/app/client/app.component.ts
@@ -2,6 +2,7 @@
 import { Router, NavigationEnd } from '@angular/router';
 import { ConfigService } from "../shared/services/config.service";
 import { DictaatService } from "../shared/services/dictaat.service";
+import { GoogleAnalyticsEventsService } from '../shared/services/google-analytics.service';
 @Component({
     selector: "wd-client-app",
     templateUrl: "./app.component.html",
@@ -16,13 +17,21 @@ export class AppComponent implements OnInit{
 
     constructor(
         private router: Router,
-        private configService: ConfigService) {
+        private configService: ConfigService,
+        public ga: GoogleAnalyticsEventsService) {
         //hide sidebar when navigating
         router.events.subscribe((event) => {
             this.showSidebar = false;
 
             if(event instanceof NavigationEnd) {
                 window.scrollTo(0, 0);
+                if(this.Title != null)
+                {
+                    debugger;
+                    ga.set(window.location.href)
+                    ga.send();
+                }
+                
             }
         
         });
