40f41a41f93bfdecffcd6eacbb0e4d12c0599b6f
Stijn Smulders <linksonder@gmail.com>
2018-06-08 13:02:58 +0200

    multiple projects up and running

--- projects/cms/src/main.ts
+++ projects/cms/src/main.ts
@@ -1,11 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { ClientModule } from './app/client/app.module';
+import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(ClientModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
